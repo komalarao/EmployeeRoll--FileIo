7b7533014424a654b64b58781e4ae07e890f14b6
komalarao <manoharvakamullu1999@gmail.com>
2023-02-27 18:34:21 -0800

    added:  number of employee data read through file

--- src/main/java/com/bridgelabz/employeePayrollFileIO/EmployeePayrollImplementation.java
+++ src/main/java/com/bridgelabz/employeePayrollFileIO/EmployeePayrollImplementation.java
@@ -9,65 +9,82 @@ import java.util.List;
 import java.util.Scanner;
 
 public class EmployeePayrollImplementation {
-	 
-	 private List<EmployeePayRollData> employePayrollList = new ArrayList<EmployeePayRollData>();
-	    Scanner scanner = new Scanner(System.in);
-	    private static final String FILE_PATH = "C:\\Users\\MY PC\\Desktop\\PayRoll.txt";
+	private List<EmployeePayRollData> employeePayrollList = new ArrayList<EmployeePayRollData>();
+    Scanner scanner = new Scanner(System.in);
+    private static final String FILE_PATH = "C:\\Users\\Desktop";
 
-	    public void readEmployeeDataFromConsole() {
-	        System.out.println("Enter Employee Id");
-	        int id = scanner.nextInt();
-	        System.out.println("Enter Employee Name");
-	        String Name = scanner.next();
-	        System.out.println("Enter the salary");
-	        int salary = scanner.nextInt();
-	        employePayrollList.add(new EmployeePayRollData(id, Name, salary));
-	    }
+    /*
+     * Creating method to print data
+     */
+    public void readEmployeeDataFromConsole() {
+        System.out.println("Enter Employee Id");
+        int id = scanner.nextInt();
+        System.out.println("Enter Employee Name");
+        String Name = scanner.next();
+        System.out.println("Enter the salary");
+        int salary = scanner.nextInt();
+        employeePayrollList.add(new EmployeePayRollData(id, Name, salary));
+    }
 
-	    public void writeEmployeeDataInConsole() {
-	        System.out.println("Writing Employee Pay Roll Data \n" + employePayrollList);
-	    }
+    public void writeEmployeeDataInConsole() {
+        System.out.println("Writing Employee Pay Roll Data \n" + employeePayrollList);
+    }
 
-	    public void addEmployee(EmployeePayRollData employee) {
-	        employePayrollList.add(employee);
-	    }
+    public void addEmployee(EmployeePayRollData employee) {
+        employeePayrollList.add(employee);
+    }
 
-	    public void writeEmployeeDataToFile() {
-	        checkFile();
-	        StringBuffer empBuffer = new StringBuffer();
-	        employePayrollList.forEach(employee -> {
-	            String employeeDataString = employee.toString().concat("\n");
-	            empBuffer.append(employeeDataString);
-	        });
-	        try {
-	            Files.write(Paths.get(FILE_PATH), empBuffer.toString().getBytes());
-	        } catch (IOException e) {
-	            e.printStackTrace();
-	        }
-	    }
+    public void writeEmployeeDataToFile() {
+        checkFile();
+        StringBuffer empBuffer = new StringBuffer();
+        employeePayrollList.forEach(employee -> {
+            String employeeDataString = employee.toString().concat("\n");
+            empBuffer.append(employeeDataString);
+        });
+        try {
+            Files.write(Paths.get(FILE_PATH), empBuffer.toString().getBytes());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
-	    // method to create file if file doesn't exist
-	    private void checkFile() {
-	        File file = new File(FILE_PATH);
-	        try {
-	            // checking file already exists
-	            if (!file.exists()) {
-	                // if not creating a new file
-	                file.createNewFile();
-	                System.out.println("Created a file at " + FILE_PATH);
-	            }
-	        } catch (IOException e1) {
-	            System.err.println("Problem encountered while creating a file");
-	        }
-	    }
+    private void checkFile() {
+        File file = new File(FILE_PATH);
+        try {
+            if (!file.exists()) {
+                file.createNewFile();
+                System.out.println("Created a file at " + FILE_PATH);
+            }
+        } catch (IOException e1) {
+            System.err.println("Problem encountered while creating a file");
+        }
+    }
 
-	    public long countEntries() {
-	        long entries = 0;
-	        try {
-	            entries = Files.lines(new File(FILE_PATH).toPath()).count();
-	        } catch (Exception e) {
-	            e.printStackTrace();
-	        }
-	        return entries;
-	    }
-}
+    public long countEntries() {
+        long entries = 0;
+        try {
+            entries = Files.lines(new File(FILE_PATH).toPath()).count();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return entries;
+    }
+
+    public void printData() {
+        try {
+            Files.lines(Paths.get(FILE_PATH)).forEach(System.out::println);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public long readDataFromFile() {
+        try {
+            String data = Files.readString(Paths.get(FILE_PATH));
+            System.out.println(data);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return countEntries();
+    }
+	 }
